bea29cb2e46fe3fbf75f870865d92c516490b7c6
NaohiroSegawa <segayan3@gmail.com>
2017-01-19 11:46:09 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,28 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false } # case_sensitiveは大文字小文字の区別をするかどうか
   has_secure_password
+  
+  # あるuserがfollowしている人の一覧
+  has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
+  
+  # あるuserをfollowしている人の一覧
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follwer_users, through: :follower_relationships, source: :follower
+  
+  # 他のユーザーをフォローする
+  def follow(other_user)
+    following_relationships.find_or_create_by(followed_id: other_user.id)
+  end
+  
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationship = following_relationships.find_by(followed_id: other_user.id)
+    following_relationship.destroy if following_relationship
+  end
+  
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 end
