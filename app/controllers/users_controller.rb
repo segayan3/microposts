7a67494bf66b2dcadff889cd8ab42641f6d34bad
NaohiroSegawa <segayan3@gmail.com>
2017-01-30 06:58:02 +0000

    Lesson7 latest version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功したらプロフィールページにリダイレクト
+      redirect_to user_path, notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面に戻る
+      render 'edit'
+    end
+  end
   
   def followings
     @user = User.find(params[:id])
